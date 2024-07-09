27b381cfd89a03922a5e856e977d6d25817db6fb
Mikegasti <avelpogi10@gmail.com>
2024-07-09 15:56:01 +0800

    first commit

---
+++ dist/js/slider.js
@@ -0,0 +1,14 @@
+var slider = tns({
+container: ".review__slider",
+items:1,
+gutter:100,
+speed:200,
+slideby:"page",
+autplay: true,
+controls: false,
+nav: false,
+autoplayButtonOutput:false,
+
+
+
+})
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        poppins:"poppins"
+      },
+      colors:{
+        primary:"#B23920",
+        secondary:"#F7B754",
+        tertiary:"#223c5F",
+      },
+      backgroundImage:{
+        
+      }
+     
+    },
+  },
+  plugins: [],
+}
+
